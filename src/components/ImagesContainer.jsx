4433cf3887d0aff66402c3dcfd14d04733d09c5d
unwantet <darkblezer59@gmail.com>
2024-05-16 23:52:46 +0500

    ref

--- src/components/ImagesContainer.jsx
+++ src/components/ImagesContainer.jsx
@@ -1,4 +1,6 @@
 import { useState } from 'react';
+import { FcNext } from "react-icons/fc";
+
 
 const ImagesContainer = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -48,23 +50,26 @@ const ImagesContainer = () => {
             </div>
       {isOpen && (
         <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-          <div className="relative bg-white p-8 rounded shadow-lg w-full max-w-2xl">
+          <div className="relative p-8 rounded  w-full max-w-2xl">
             <button
               className="absolute top-2 right-2 text-2xl font-bold text-gray-600 hover:text-gray-800"
               onClick={closeModal}
             >
-              &times;
+              <span className='hover:text-orange-600 font-bold transition-all duration-300 text-white'>x</span>
             </button>
-            <div className="modal-body flex flex-col items-center">
+            <div className="modal-body flex flex-col items-center px-5">
               <div className="slider relative w-full flex items-center">
                 <button
-                  className="absolute left-0 text-3xl font-bold text-gray-600 hover:text-gray-800"
+                  className="absolute left-0 text-3xl font-bold text-gray-600 hover:text-gray-800 mr-5"
                   onClick={handlePrev}
                 >
-                  &#10094;
+                    <div className='rounded-full bg-white w-14 h-14 flex items-center justify-center rotate-180'>
+                    <FcNext />
+
+                    </div>
                 </button>
                 <img
-                  className="w-full max-h-96 object-contain"
+                  className="w-full max-h-[500px]  rounded-xl object-contain"
                   src={images[currentSlideIndex]}
                   alt="Product"
                 />
@@ -72,14 +77,17 @@ const ImagesContainer = () => {
                   className="absolute right-0 text-3xl font-bold text-gray-600 hover:text-gray-800"
                   onClick={handleNext}
                 >
-                  &#10095;
+                  <div className='rounded-full bg-white w-14 h-14 flex items-center justify-center '>
+                    <FcNext />
+
+                    </div>
                 </button>
               </div>
-              <div className="thumbnails flex mt-4 space-x-2">
+              <div className="thumbnails flex mt-4 gap-4 justify-center">
                 {images.map((image, index) => (
                   <img
                     key={index}
-                    className={`w-16 h-16 object-cover cursor-pointer border ${index === currentSlideIndex ? 'border-blue-500' : 'border-transparent'} rounded`}
+                    className={`w-20 h-20 object-cover cursor-pointer border ${index === currentSlideIndex ? 'border-blue-500' : 'border-transparent'} rounded`}
                     src={image}
                     alt={`Thumbnail ${index + 1}`}
                     onClick={() => showSlide(index)}
